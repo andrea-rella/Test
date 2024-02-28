41f88ae98264cc46e28fdb0f4633666b513cdeb4
Andrea Rella <andyre0112@gmail.it>
2024-02-28 11:23:38 +0100

    Your commit message here

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "vector"
+#include "string"
+#include "memory"
+#include "map"
+#include "limits"
+
+class Base{
+    int i=0;
+public:
+
+     virtual void print(){
+        std::cout << "Base method" << std::endl;
+    }
+
+};
+
+class Derived: public Base{
+    void print() override{
+        std::cout << "Derived method" << std::endl;
+    }
+};
+
+void usePrint(Base& b){
+    b.print();
+};
+
+void usePrint_notref(Base b){
+    b.print();
+};
+
+int main() {
+
+    Derived d;
+    Base* b_ptr = &d;
+    std::vector<Base> vB;
+    vB.push_back(d);
+
+    usePrint(d); // prints derived method (because the method is overridden,
+                    // dynamic binding if it wasn't overriden it would print base)
+    usePrint_notref(d); // Base method (slice down happen)
+    b_ptr->print(); // prints derived method (because the method is overridden, dynamic binding)
+                    // dynamic binding if it wasn't overriden it would print base)
+    vB[0].print();  // Base method (slice down happens)
+
+}
+
+
